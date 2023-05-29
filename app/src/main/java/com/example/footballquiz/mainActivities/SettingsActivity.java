732483c6f58d692995165d51c265a58f608f2801
rubenchilingaryan <chilingaryanruben56@gmail.com>
2023-05-29 18:56:29 +0400

    SettingsActivity works
    Signed-off-by: rubenchilingaryan <chilingaryanruben56@gmail.com>

--- app/src/main/java/com/example/footballquiz/mainActivities/SettingsActivity.java
+++ app/src/main/java/com/example/footballquiz/mainActivities/SettingsActivity.java
@@ -5,15 +5,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.footballquiz.R;
 import com.example.footballquiz.authorization.LoginActivity;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class SettingsActivity extends AppCompatActivity {
 
     ImageView signOut,back;
+    EditText username;
+    Button changeUsername;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +31,8 @@ public class SettingsActivity extends AppCompatActivity {
 
         signOut = findViewById(R.id.btnSignOutSettings);
         back = findViewById(R.id.backButtonSettings);
+        username = findViewById(R.id.username_settings);
+        changeUsername = findViewById(R.id.changeUsernameBtn);
 
         signOut.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -37,6 +48,31 @@ public class SettingsActivity extends AppCompatActivity {
             public void onClick(View v) {
                 startActivity(new Intent(getApplicationContext(), ProfileActivity.class));
                 finish();
+            }
+        });
+
+        FirebaseFirestore firestore = FirebaseFirestore.getInstance();
+        String userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        DocumentReference documentRef = firestore.collection("users").document(userId);
+
+        documentRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                String currentUsername = documentSnapshot.getString("Username");
+                username.setText(currentUsername);
+            }
+        });
+
+        changeUsername.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String newUsername = username.getText().toString();
+                documentRef.update("Username",newUsername).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        Toast.makeText(SettingsActivity.this, "Successfully changed", Toast.LENGTH_SHORT).show();
+                    }
+                });
             }
         });
     }
